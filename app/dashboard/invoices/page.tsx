e62f3ea6d4f6ff951d7cf5b9be38e6b7eca00533
Ed Anisko <ed.anisko@gmail.com>
2024-10-25 12:04:55 -0700

    chapter 16 - metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from "next";
 import Pagination from "@/app/ui/invoices/pagination";
 import { fetchInvoicesPages } from '@/app/lib/data';
 import Search from "@/app/ui/search";
@@ -7,6 +8,9 @@ import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
 
+export const metadata: Metadata = {
+  title: 'Invoices',
+}
 
 export default async function Page(props: {
   searchParams?: Promise<{
